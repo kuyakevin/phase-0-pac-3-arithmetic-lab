0f0a3b93f3aa3349594a7503e3872a840eb28e46
Kevin Chaney <kevin_chaney@icloud.com>
2022-03-23 16:40:30 -0700

    commit tree 487da52749c2cdbdaeb0853a9b9c85eae7685fa3

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+    console.log(add(a, b));
+}
+
+function subtract(a, b) {
+    return a - b;
+    console.log(subtract(a, b));
+}
+
+function multiply(a, b) {
+    return a * b; 
+    console.log(multiply(a, b));
+}
+
+function divide(a, b) {
+    return a / b;
+    console.log(divide(a, b));
+}
+
+function increment(n) {
+    n++
+    return (n);
+}
+
+function decrement(n) {
+    n--
+    return (n);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
